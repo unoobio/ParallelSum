8c852aee81b6e570de8eae4867ec76d8695af662
Alexander Voronin <Unoobio@gmail.com>
2023-03-12 00:41:20 +0700

    Add project files.

---
+++ ParallelSum/Benchmark.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace ParallelSum
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.Method)]
+    [RankColumn]
+    [SimpleJob(RunStrategy.Monitoring, warmupCount: 1, launchCount: 1, iterationCount: 5)]
+    public class Benchmark
+    {
+        private List<double> NumbersCollection { get; set; }
+
+        [Params(100000, 1000000, 10000000)]
+        public int CollectionSize { get; set; }
+
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            NumbersCollection = new List<double>();
+
+            // Initialize the arrays with random values
+            var rand = new Random();
+            for (int i = 0; i < CollectionSize; i++)
+            {
+                NumbersCollection.Add(rand.Next(0, 1000));
+            }
+        }
+
+        [Benchmark]
+        public void SimpleSum()
+        {
+            SumCalculator.Sum(NumbersCollection);
+        }
+
+        [Benchmark]
+        public void SumManualy()
+        {
+            SumCalculator.SumManualy(NumbersCollection);
+        }
+
+        [Benchmark]
+        public void SumForeach()
+        {
+            SumCalculator.SumForeach(NumbersCollection);
+        }
+
+        [Benchmark]
+        [Arguments(1)]
+        [Arguments(2)]
+        [Arguments(4)]
+        [Arguments(8)]
+        public void SumWithParallelLINQ(int batchesCount)
+        {
+            SumCalculator.SumWithParallelLINQ(NumbersCollection, batchesCount);
+        }
+
+        [Benchmark]
+        [Arguments(2)]
+        [Arguments(4)]
+        [Arguments(8)]
+        public void SumWithThread(int threadsCount)
+        {
+            SumCalculator.SumWithThreads(NumbersCollection, threadsCount);
+        }
+    }
+}
---
+++ ParallelSum/SumCalculator.cs
@@ -0,0 +1,88 @@
+﻿namespace ParallelSum
+{
+    internal class SumCalculator
+    {
+        public static double Sum(List<double> numbers)
+            => numbers.Sum();
+
+        public static double SumManualy(List<double> numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        public static double SumForeach(List<double> numbers)
+        {
+            double sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+
+        public static double SumWithThreads(List<double> numbers, int threadsCount)
+        {
+            int batchCount = numbers.Count / threadsCount + 1;
+            
+            double totalSum = 0;
+            object lockObject = new object();
+            Barrier barrier = new Barrier(threadsCount + 1);
+            for (int i = 0; i < threadsCount; i++)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    int index = i;
+                    double batchSum = GetSumFromBatch(batchCount, index, numbers);
+                    lock (lockObject)
+                    {
+                        totalSum += batchSum;
+                    }
+                    barrier.SignalAndWait();
+                });
+                thread.Start();
+            }
+            
+            barrier.SignalAndWait();
+            return totalSum;
+        }
+
+        public static double SumWithParallelLINQ(List<double> numbers, int batchesCount)
+        {
+            int batchCount = numbers.Count / batchesCount + 1;
+
+            double totalSum = 0;
+            object lockObject = new object();
+            Parallel.For(0, batchesCount, (index) =>
+            {
+                double batchSum = GetSumFromBatch(batchCount, index, numbers);
+                lock (lockObject)
+                {
+                    totalSum += batchSum;
+                }
+            });
+            return totalSum;
+        }
+
+        private static double GetSumFromBatch(int batchCount, int batchIndex, List<double> numbers) 
+        {
+            int startIndex = batchIndex * batchCount;
+            int endIndex = startIndex + batchCount;
+            if (endIndex > numbers.Count)
+                endIndex = numbers.Count;
+
+            double batchSum = 0;
+            for (int j = startIndex; j < endIndex; j++)
+            {
+                batchSum += numbers[j];
+            }
+
+            return batchSum;
+        }
+    }
+}
